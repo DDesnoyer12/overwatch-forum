1a62f255e8bbf17edb82b82b2dbde690f84da638
Richard <richard.j.schaefer@wmich.edu>
2017-11-04 20:08:43 -0400

    Made Character Changes
    Added in images for the 26 Characters, EXCEPT for Doomfist and Moira (Missing Renders for both)

--- Characters.php
+++ Characters.php
@@ -13,37 +13,35 @@
 	<section>
 		<div class="character-div">
 			<ul class="characters">
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
-				<li><img src = "images/characters/full-portrait.png"/><p>TORBJORN</p></li>
+				
+				<li><img src = "images/characters/renders/Doomfist.png"/><p>Doomfist (MISSING)</p></li>
+				<li><img src = "images/characters/renders/Genji.png"/><p>Genji</p></li>
+				<li><img src = "images/characters/renders/McCree.png"/><p>McCree</p></li>
+				<li><img src = "images/characters/renders/Pharah.png"/><p>Pharah</p></li>
+				<li><img src = "images/characters/renders/Reaper.png"/><p>Reaper</p></li>
+				<li><img src = "images/characters/renders/Soldier76.png"/><p>Soldier: 76</p></li>
+				<li><img src = "images/characters/renders/Sombra.png"/><p>Sombra</p></li>
+				<li><img src = "images/characters/renders/Tracer.png"/><p>Tracer</p></li>
+				<li><img src = "images/characters/renders/Bastion.png"/><p>Bastion</p></li>
+				<li><img src = "images/characters/renders/Hanzo.png"/><p>Hanzo</p></li>
+				<li><img src = "images/characters/renders/Junkrat.png"/><p>Junkrat</p></li>
+				<li><img src = "images/characters/renders/Mei.png"/><p>Mei</p></li>
+				<li><img src = "images/characters/renders/Torbjorn.png"/><p>Torbjorn</p></li>
+				<li><img src = "images/characters/renders/Widowmaker.png"/><p>Widowmaker</p></li>
+				<li><img src = "images/characters/renders/D.Va.png"/><p>D.Va</p></li>
+				<li><img src = "images/characters/renders/Orisa.png"/><p>Orisa</p></li>
+				<li><img src = "images/characters/renders/Reinhardt.png"/><p>Reinhardt</p></li>
+				<li><img src = "images/characters/renders/Roadhog.png"/><p>Roadhog</p></li>
+				<li><img src = "images/characters/renders/Winston.png"/><p>Winston</p></li>
+				<li><img src = "images/characters/renders/Zarya.png"/><p>Zarya</p></li>
+				<li><img src = "images/characters/renders/Ana.png"/><p>Ana</p></li>
+				<li><img src = "images/characters/renders/Lucio.png"/><p>Lucio</p></li>
+				<li><img src = "images/characters/renders/Mercy.png"/><p>Mercy</p></li>
+				<li><img src = "images/characters/renders/Moira.png"/><p>Moira (MISSING)</p></li>
+				<li><img src = "images/characters/renders/Symmetra.png"/><p>Symmetra</p></li>
+				<li><img src = "images/characters/renders/Zenyatta.png"/><p>Zenyatta</p></li>
+				
+				
 			</ul>
 		</div>
 	</section>

4c0de4bf15756ef4a7d268ecfc0fd8b82eafbe44
Richard <richard.j.schaefer@wmich.edu>
2017-11-09 17:24:36 -0500

    Its making me push
    Why you do this to me GitHub

--- Characters.php
+++ Characters.php
@@ -14,6 +14,7 @@
 		<div class="character-div">
 			<ul class="characters">
 				
+				
 				<li><img src = "images/characters/renders/Doomfist.png"/><p>Doomfist</p></li>
 				<li><img src = "images/characters/renders/Genji.png"/><p>Genji</p></li>
 				<li><img src = "images/characters/renders/McCree.png"/><p>McCree</p></li>
@@ -22,18 +23,24 @@
 				<li><img src = "images/characters/renders/Soldier76.png"/><p>Soldier: 76</p></li>
 				<li><img src = "images/characters/renders/Sombra.png"/><p>Sombra</p></li>
 				<li><img src = "images/characters/renders/Tracer.png"/><p>Tracer</p></li>
+				
+				
 				<li><img src = "images/characters/renders/Bastion.png"/><p>Bastion</p></li>
 				<li><img src = "images/characters/renders/Hanzo.png"/><p>Hanzo</p></li>
 				<li><img src = "images/characters/renders/Junkrat.png"/><p>Junkrat</p></li>
 				<li><img src = "images/characters/renders/Mei.png"/><p>Mei</p></li>
 				<li><img src = "images/characters/renders/Torbjorn.png"/><p>Torbjorn</p></li>
 				<li><img src = "images/characters/renders/Widowmaker.png"/><p>Widowmaker</p></li>
+				
+				
 				<li><img src = "images/characters/renders/D.Va.png"/><p>D.Va</p></li>
 				<li><img src = "images/characters/renders/Orisa.png"/><p>Orisa</p></li>
 				<li><img src = "images/characters/renders/Reinhardt.png"/><p>Reinhardt</p></li>
 				<li><img src = "images/characters/renders/Roadhog.png"/><p>Roadhog</p></li>
 				<li><img src = "images/characters/renders/Winston.png"/><p>Winston</p></li>
 				<li><img src = "images/characters/renders/Zarya.png"/><p>Zarya</p></li>
+				
+				
 				<li><img src = "images/characters/renders/Ana.png"/><p>Ana</p></li>
 				<li><img src = "images/characters/renders/Lucio.png"/><p>Lucio</p></li>
 				<li><img src = "images/characters/renders/Mercy.png"/><p>Mercy</p></li>

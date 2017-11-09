fdd25057cf8632b92d6ec85e84cce5f0d8a02e6f
Daniel <daniel.s.desnoyer@wmich.edu>
2017-11-09 12:36:28 -0500

    Made Changes to Characters.php
    Made some changes to Characters.php and probably some other sutff too

--- includes/final-header.php
+++ includes/final-header.php
@@ -1,7 +1,7 @@
 <header>
 		<img id="logo" src="images/overwatchlogo.png"/>
 			<ul>
-				<li><a href="Final project.php">Home</a></li>
+				<li><a href="Home.php">Home</a></li>
 				<li><a href="Characters.php">Characters</a></li>
 				<li><a href="Maps.php">Maps</a></li>
 				<li><a href="Tiers.php">Tier List</a></li>

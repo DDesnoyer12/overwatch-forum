85bfc235a7fc8f1186391d7a4aff2f6d2abb05de
Richard <richard.j.schaefer@wmich.edu>
2017-11-29 18:08:13 -0500

    RJ's updates
    Worked on tier list, added some home page stuff

--- Tiers.php
+++ Tiers.php
@@ -5,13 +5,187 @@
 	<title>An Overwatch Fan Website</title>
 	<link rel="stylesheet" href="styles/reset.css" />
 	<link rel="stylesheet" href="styles/css-header.css" />
+	<link rel="stylesheet" href="styles/css-tiers.css" />
 	<link rel="icon" href="images/favicon.png" />
 </head>
 <body>
 <?php include 'includes/final-header.php'; ?>
 <article>
 	<section>
-		<h1>TIER LIST</h1>
+	
+		<h1>Competitve Ranks</h1>
+		<img class ="pic" src="images/comp-ranks.png"></img><br />
+		<br />
+		<h2>"I need healing" - Genji Shimada</h2>
+		<br /><hr><br />
+		<h1>TIERS: NOVEMBER 2017</h1>
+		<div id="character-tier">
+		<table>
+			<tr>
+				<th class="thead">Tiers</th>
+				<th class="thead">Heroes</th>
+				<th class="thead">Roles</th>
+				<th class="thead">Difficulties</th>
+			</tr>
+			<tr>
+				<td class="one">Tier 1</td>
+				<td>Lucio</td>
+				<td class="support">Support</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="one">Tier 1</td>
+				<td>Winston</td>
+				<td class="tank">Tank</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="two">Tier 2</td>
+				<td>Soldier: 76</td>
+				<td class="offense">Offense</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="two">Tier 2</td>
+				<td>Reaper</td>
+				<td class="offense">Offense</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="two">Tier 2</td>
+				<td>Junkrat</td>
+				<td class="defense">Defense</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="two">Tier 2</td>
+				<td>Ana</td>
+				<td class="support">Support</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="two">Tier 2</td>
+				<td>Mercy</td>
+				<td class="support">Support</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="two">Tier 2</td>
+				<td>Zenyatta</td>
+				<td class="support">Support</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Hanzo</td>
+				<td class="defense">Defense</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Doomfist</td>
+				<td class="offense">Offense</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Genji</td>
+				<td class="offense">Offense</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>McCree</td>
+				<td class="offense">Offense</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Pharah</td>
+				<td class="offense">Offense</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Tracer</td>
+				<td class="offense">Offense</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Moira</td>
+				<td class="support">Support</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>D.Va</td>
+				<td class="tank">Tank</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Reinhardt</td>
+				<td class="tank">Tank</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Roadhog</td>
+				<td class="tank">Tank</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="three">Tier 3</td>
+				<td>Zarya</td>
+				<td class="tank">Tank</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="four">Tier 4</td>
+				<td>Mei</td>
+				<td class="defense">Defense</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="four">Tier 4</td>
+				<td>Orisa</td>
+				<td class="tank">Tank</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="five">Tier 5</td>
+				<td>Bastion</td>
+				<td class="defense">Defense</td>
+				<td>1</td>
+			</tr>
+			<tr>
+				<td class="five">Tier 5</td>
+				<td>Torbjorn</td>
+				<td class="defense">Defense</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="five">Tier 5</td>
+				<td>Widowmaker</td>
+				<td class="defense">Defense</td>
+				<td>2</td>
+			</tr>
+			<tr>
+				<td class="five">Tier 5</td>
+				<td>Sombra</td>
+				<td class="offense">Offense</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td class="five">Tier 5</td>
+				<td>Symmetra</td>
+				<td class="support">Support</td>
+				<td>2</td>
+			</tr>
+		</table>
+		
+		</div>
 		
 	</section>
 </article>
